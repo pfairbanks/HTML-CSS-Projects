6476b12ae71b209bcfa4f21aaed96895957bf31a
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-04-05 19:30:26 -0700

    Add Content
    Added img element and text attributes

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+alert("This is my JavaScript Alert Box");
+function My_First_Function() {
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
